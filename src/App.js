32fa5fceec25051023df7b258ec8128b0c26e71e
charrrleee <charlcpy@gmail.com>
2023-04-13 21:49:02 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
+import { Icon, Input } from '@mui/material';
+import { useEffect, useState } from 'react';
+import userIcon from './assets/user.png';
+import botIcon from './assets/tesla.png';
 
-function App() {
+const App = () =>  {
+  const [messages, setMessages] = useState([
+    {
+      isUser: false,
+      message: 'Hi there!',
+    },
+  ]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleInputSubmit = (event) => {
+    event.preventDefault();
+
+    if (inputValue.trim() === '') {
+      return;
+    }
+
+    const newMessage = {
+      isUser: true,
+      message: inputValue,
+    };
+
+
+    const newResponseMessage = {
+      isUser: false,
+      message: "response hi ",
+    };
+
+    setMessages((prevMessages) => [...prevMessages, newMessage]);
+    setMessages((prevMessages) => [...prevMessages, newResponseMessage]);
+    setInputValue('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index} className={message.isUser ? "user-message" : "chatbot-message" }>
+            <div className="message-container">
+              <img src={message.isUser ? userIcon : botIcon} alt={message.isUser ? 'User' : 'Chatbot'} className="icon" style={{ width: '20px', height: '20px', margin: '10px 10px'}} />
+              <p style={{alignSelf: "center"}}>{message.message}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+      <form onSubmit={handleInputSubmit} className="input-form">
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button type="submit" className="send-button">
+          <SendIcon />
+        </button>
+      </form>
     </div>
   );
 }
